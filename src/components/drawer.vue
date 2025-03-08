115c2c8155ec4b06144c6fe2a0c3fe928d7271b7
marc.zamora <marc@bcnbcn.io>
2025-03-08 11:16:19 +0100

    menu dial - no translates

--- src/components/drawer.vue
+++ src/components/drawer.vue
@@ -1,41 +1,36 @@
 <template>
 
-  <div data-dial-init class="fixed z-40 end-8 bottom-20 group">
-    <div id="speed-dial-menu-text-outside-button" class="flex flex-col items-end hidden mb-4 space-y-2">
-      <a href="#home"
-        class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
-        #1 {{ $t('navbar.home-title') }}
-      </a>
-      <a href="#about"
-        class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
-      2#  {{ $t('navbar.about-title') }}
-      </a>
-      <a href="#portfolio"
-        class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
-        3# {{ $t('navbar.portfolio-title') }}
-      </a>
-      <a href="#customers"
-        class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
-        4# {{ $t('navbar.customers-title') }}
-      </a>
-      <a href="#skills"
-        class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
-        5# {{ $t('navbar.skills-title') }}
-      </a>
-    </div>
-    <button type="button" data-dial-toggle="speed-dial-menu-text-outside-button" aria-controls="speed-dial-menu-text-outside-button" aria-expanded="false" 
-      class="fixed end-8  bottom-8 flex items-center justify-center text-white hover:text-yellow-300 bg-black rounded-lg w-14 h-14 hover:bg-gray-950">
-      <svg class="w-5 h-5 transition-transform group-hover:rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-      </svg>
-      <span class="sr-only">Open actions menu</span>
-    </button>
+<div data-dial-init class="fixed z-40 end-6 top-20 group">
+  <button type="button" data-dial-toggle="speed-dial-menu-top-right" aria-controls="speed-dial-menu-top-right" aria-expanded="false" 
+    class="fixed end-6 top-6 w-14 h-14 flex items-center justify-center text-white bg-black hover:bg-gray-950 rounded-full">
+    <svg class="w-5 h-5 transition-transform group-hover:rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+    </svg>
+    <span class="sr-only">Open actions menu</span>
+  </button>
+  <div id="speed-dial-menu-top-right" class="flex flex-col items-end hidden mt-4 space-y-2">
+    <a href="#home"
+      class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
+      #1 {{ $t('navbar.home-title') }}
+    </a>
+    <a href="#about"
+      class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
+    2#  {{ $t('navbar.about-title') }}
+    </a>
+    <a href="#portfolio"
+      class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
+      3# {{ $t('navbar.portfolio-title') }}
+    </a>
+    <a href="#customers"
+      class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
+      4# {{ $t('navbar.customers-title') }}
+    </a>
+    <a href="#skills"
+      class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
+      5# {{ $t('navbar.skills-title') }}
+    </a>
   </div>
-
-
-
-
-
+</div>
 
 <!--   
   <div class="fixed top-4 right-4 md:top-8 md:right-8 z-40">
