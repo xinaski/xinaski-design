bd9919a6f0363e8a87e5e0284411e9e911a9ceaa
marc.zamora <marc@bcnbcn.io>
2025-03-10 11:17:41 +0100

    menu responsive

--- src/components/drawer.vue
+++ src/components/drawer.vue
@@ -2,13 +2,13 @@
 
 <div data-dial-init class="fixed z-40 end-6 top-20 group">
   <button @click="toggleMenu"
-    class="fixed end-6 top-6 w-14 h-14 flex items-center justify-center text-white bg-black hover:bg-gray-950 rounded-full cursor-pointer">
+    class="fixed end-3 top-3 md:end-6 md:top-6 w-14 h-14 flex items-center justify-center text-white bg-black hover:bg-gray-950 rounded-full cursor-pointer">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
     <span class="sr-only">Open actions menu</span>
   </button>
-  <div  :class="{ 'hidden': !isMenuOpen }" class="flex flex-col items-end mt-4 space-y-2">
+  <div :class="{ 'hidden': !isMenuOpen }" class="flex flex-col items-end mt-0 space-y-2">
     <a href="#home" @click="closeMenu"
       class="relative py-3 px-6 bg-black hover:bg-gray-950 text-white hover:text-yellow-300 font-stretch-extra-expanded text-right border border-black rounded-full shadow-xs ">
       #1 {{ $t('navbar.home-title') }}
